3eb34617240e8b472a905e572ae5e58d24a2fd6f
daianjimax <daianji@gmail.com>
2021-11-06 15:02:50 +0900

    TinyCoreApplication append instead of WicketApplication

--- src/test/java/tiny/core/component/TestHomePage.java
+++ src/test/java/tiny/core/component/TestHomePage.java
@@ -1,32 +1,28 @@
 package tiny.core.component;
 
-import tiny.core.component.HomePage;
-import tiny.core.WicketApplication;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import tiny.core.WicketApplication;
+import tiny.core.TinyCoreApplication;
 
 /**
  * Simple test using the WicketTester
  */
-public class TestHomePage
-{
-	private WicketTester tester;
+public class TestHomePage {
 
-	@BeforeEach
-	public void setUp()
-	{
-		tester = new WicketTester(new WicketApplication());
-	}
+    private WicketTester tester;
 
-	@Test
-	public void homepageRendersSuccessfully()
-	{
-		//start and render the test page
-		tester.startPage(HomePage.class);
+    @BeforeEach
+    public void setUp() {
+        tester = new WicketTester(new TinyCoreApplication());
+    }
 
-		//assert rendered page class
-		tester.assertRenderedPage(HomePage.class);
-	}
+    @Test
+    public void homepageRendersSuccessfully() {
+        //start and render the test page
+        tester.startPage(HomePage.class);
+
+        //assert rendered page class
+        tester.assertRenderedPage(HomePage.class);
+    }
 }
